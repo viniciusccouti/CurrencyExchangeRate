122223eb64a59b789cdf73345fc633bd6d95cc48
Vinicius Coutinho <viniciuscoutinho@LTBRA-M046.local>
2024-05-25 15:09:59 -0300

    update

---
+++ main.py
@@ -0,0 +1,7 @@
+#janela - 500 x 500
+#titulo
+#campos de selecionar moedas de origem e destino(campos de listas) com labels Selecione a moeda de origem
+#botao de "converter"
+#lista de exibicao com os nomes das moedas
+
+import customtkinter
